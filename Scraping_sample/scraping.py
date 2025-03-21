dbb26cd9098bcbfa523c0de84c9715c0763a966d
Yosuke Mori <y_mori@dyayd.com>
2025-03-21 10:22:58 +0900

    feat:scraping_RCL修正。playwrightを導入。

--- Scraping_sample/scraping.py
+++ Scraping_sample/scraping.py
@@ -1,16 +1,22 @@
 from bs4 import BeautifulSoup as bs4
-import requests
+from playwright.sync_api import sync_playwright
 from lxml import html
 import pandas as pd
+import datetime
 
 def scraping_RCL():
   url = "https://recruit-holdings.com/ja/newsroom/"
-  res = requests.get(url)
 
-  soup = bs4(res.text, "html.parser")
+  with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto(url, wait_until="networkidle")
+    res = page.content()
+    browser.close()
 
+  soup = bs4(res, "html.parser")
   lxml_data = html.fromstring(str(soup))
-  article_list = lxml_data.xpath("//div[@id='notifications']//p[contains(@class, 'C102_Text')]")
+  article_list = lxml_data.xpath("//div[contains(@id, 'news')]//p[contains(@class, 'C102_Text')]")
 
   article_columns = dict(date = [], articleName = [])
   columns = ["date", "articleName"]
@@ -23,5 +29,5 @@ def scraping_RCL():
     df_article = pd.concat([df_article, df_article_set], ignore_index=True)
   df_article["date"] = pd.to_datetime(df_article["date"], format="%Y年%m月%d日")
 
-  target_article = df_article.loc[df_article["date"]==pd.to_datetime("20250303")]
+  target_article = df_article.loc[df_article["date"]==pd.to_datetime(datetime.datetime.now().strftime("%Y-%m-%d"))]
   return target_article
