b0602fdf82eb656e6957946701e472a178edfba9
Yosuke Mori <y_mori@dyayd.com>
2025-03-23 06:05:31 +0900

    feat:sendGmailFB対応中。途中Push。

--- Mailer_sample/mailer_gmail.py
+++ Mailer_sample/mailer_gmail.py
@@ -2,15 +2,16 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import formatdate
 
-def send_gmail(send_adress, password, mail_components):
+def send_gmail(send_address, password, mail_components):
   smtpobj = smtplib.SMTP("smtp.gmail.com", 587)
   smtpobj.starttls()
-  smtpobj.login(send_adress, password)
+  smtpobj.login(send_address, password)
 
-  msg = MIMEText(mail_components["body"])
-  msg["Subject"] = mail_components["subject"]
+  msg = MIMEText(mail_components.get("body", ""))
+  msg["Subject"] = mail_components.get("subject", "No Subject")
   msg["From"] = mail_components["from"]
   msg["To"] = mail_components["to"]
+  msg["Cc"] = mail_components["cc"]
   msg["Date"] = formatdate()
 
   smtpobj.send_message(msg)
